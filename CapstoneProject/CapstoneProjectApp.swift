dd33ec8e9ec4991ca930e3b5f6714f78f8ae9645
Jacob01984 <Jacoblavenant@yahoo.com>
2023-10-09 12:16:36 -0500

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by Jacob Lavenant on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
